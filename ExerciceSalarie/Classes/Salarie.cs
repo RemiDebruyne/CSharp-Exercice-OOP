c32d842b04f333f9bd687b9d92af8b976a855603
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-05 14:09:07 +0100

    add exerciceSalarie and exerciceWaterTank

---
+++ ExerciceSalarie/Classes/Salarie.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceSalarie.Classes
+{
+    internal class Salarie
+    {
+        private static int _nombreEmployes;
+        private static int _salaireTotale;
+        public int _salaire;
+        public static int NombreEmployes { get => _nombreEmployes; set => _nombreEmployes = value; }
+        public static int SalaireTotale { get => _salaireTotale; set => _salaireTotale = value; }
+
+        public static double MoyenneSalaire => SalaireTotale / NombreEmployes;
+        public string Nom { get; set; }
+        public int Salaire
+        {
+            get
+            {
+                return _salaire;
+            }
+            set
+            {
+                SalaireTotale = SalaireTotale - Salaire + value;
+                _salaire = value;
+
+            }
+        }
+        public string Matricule { get; set; }
+        public string Service { get; set; }
+        public string Categorie { get; set; }
+
+
+
+
+        static public void AfficherInfosEntreprises()
+        {
+            Console.WriteLine("Le nombre total de salariés est de " + NombreEmployes + " et leur salaires est de " + SalaireTotale + " euros");
+        }
+
+        public override string ToString()
+        {
+            return "Le salaire de l'employé " + Nom + " matricule "+ Matricule + " est de " + Salaire + " poutres";
+        }
+
+
+
+        public void AfficherInfosEmployes()
+        {
+            Console.WriteLine(this);
+        }
+
+        static public void AfficherMoyenneSalaire()
+        {
+            Console.WriteLine(MoyenneSalaire);
+        }
+
+
+
+        public Salarie()
+        {
+            Nom = "Jean";
+            Salaire = 8;
+            Matricule = Guid.NewGuid().ToString();
+            Service = "marketing";
+            Categorie = "cadre";
+            NombreEmployes++;
+        }
+        public Salarie(string nom, int salaire, string service, string categorie) : this()
+        {
+            Nom = nom;
+            Salaire = salaire;
+            Matricule = Guid.NewGuid().ToString();
+            Service = service;
+            Categorie = categorie;
+        }
+
+
+    }
+}
---
+++ ExerciceSalarie/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExerciceSalarie;
+using ExerciceSalarie.Classes;
+
+Salarie jean = new Salarie();
+Salarie chloe = new Salarie("Chloé", 10000, "marketing", "cadre");
+Salarie marie = new Salarie("Marie", 10000, "IT", "salarié");
+Salarie pierre = new Salarie("Pierre", 10000, "commercial", "cadre");
+
+List<Salarie> list = new List<Salarie>
+{
+    jean,
+    chloe,
+    marie,
+    pierre,
+};
+
+foreach (Salarie salarie in list)
+{
+    salarie.AfficherInfosEmployes();
+}
+
+Console.WriteLine();
+
+Salarie.AfficherInfosEntreprises();
+
+list[1].Salaire = 0;
+
+Console.WriteLine();
+chloe.AfficherInfosEmployes();
+Console.WriteLine();
+Salarie.AfficherInfosEntreprises();
+Console.WriteLine();
+Salarie.AfficherMoyenneSalaire();
